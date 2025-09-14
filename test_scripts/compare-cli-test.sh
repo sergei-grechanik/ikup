f3eb51c070d7dadbc14569696fb63f5892c0f21a
Sergei Grechanik <sergei.grechanik@gmail.com>
2025-09-14 08:14:41 -0700

    Some dep fixes

--- test_scripts/compare-cli-test.sh
+++ test_scripts/compare-cli-test.sh
@@ -9,11 +9,11 @@ if [ -z "$1" ]; then
 fi
 
 if [ -d "$1" ]; then
-    exec uv run python -m ikup.testing.output_comparison "$1" ./data/cli-test-references
+    exec uv run --no-sync python -m ikup.testing.output_comparison "$1" ./data/cli-test-references
 fi
 
 TEST_OUTPUT_FILE="$1"
 TEST_NAME=$(basename "$TEST_OUTPUT_FILE" .out)
 REFERENCE_FILE="./data/cli-test-references/${TEST_NAME}.reference"
 
-exec uv run python -m ikup.testing.output_comparison "$TEST_OUTPUT_FILE" "$REFERENCE_FILE"
+exec uv run --no-sync python -m ikup.testing.output_comparison "$TEST_OUTPUT_FILE" "$REFERENCE_FILE"
